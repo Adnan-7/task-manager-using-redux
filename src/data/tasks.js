7526e991d0ba750e0c523804ad193ff31b405898
Adnan Khan <adnan7.zain@gmail.com>
2021-07-27 18:48:56 +0500

    add initial state

---
+++ src/data/tasks.js
@@ -0,0 +1,12 @@
+export const initialTask=[
+    {
+        id:1,
+        taskTitle:"Appointment with Bob",
+        taskDateTime:'2021-07-16 9:30'
+    },
+    {
+        id:2,
+        taskTitle:"Meeting with contractor",
+        taskDateTime:'2021-08-12 11:30'
+    }
+]
